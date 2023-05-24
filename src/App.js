86fc6032ab969d658cd8770e6c19be2ed31f7671
Webmyn001 <125801312+Webmyn001@users.noreply.github.com>
2023-05-24 22:42:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,136 @@
-import React from 'react'
-import Header from './ANSlesson/Header'
+import './App.css';
+import { useEffect } from 'react';
+
+import Header from './Calculator/Components/Navbar/Header';
 import {BrowserRouter as Router , Route, Routes, } from "react-router-dom";
-import Home from './ANSlesson/Home';
-import Footer from './ANSlesson/Footer';
-import Fulldetails from './ANSlesson/Fulldetails';
-import Lessons from './ANSlesson/Lessons';
-import About from './ANSlesson/About';
-import Comment from './ANSlesson/Comment';
-import DashboardApp from './ANSdashboard/DashboardApp';
-import Post from './ANSdashboard/Post';
-import Editpost from './ANSdashboard/Editpost';
-import Comments from './ANSdashboard/Comments';
+import Home from './Calculator/Components/Home';
+
+import Getstarted from './Calculator/Components/Getstarted';
+import Gpcalc4 from './Calculator/Components/Gpcalc4';
+import Result from './Calculator/Components/Results';
+import {  useState } from 'react';
+import Finalresult from './Calculator/Components/Finalrsult';
+
+import Docs from './Calculator/Components/Docs';
+import About from './Calculator/Components/About';
+import Gpcalc5 from './Calculator/Components/Gpcalc5';
+import Gpcalc7 from './Calculator/Components/Gpcalc7';
+import ReactGA from "react-ga" ;
+ 
 
 
-
-
-
+const TRACKING_ID = "UA-235873888-3"
+ReactGA.initialize(TRACKING_ID)
 
 function App() {
-  return (
+
+   
+  useEffect( ()=> {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+   }, []);
+  
+
+  
+         
+   const [master , setMaster] = useState([])
+   
+  
+   const [calc, setcalc] = useState([])
+   const [calc2, setcalc2] = useState([])
+            
+   
+   console.log(master) 
+     const getinfo =(info) => {
+        console.log(info)
+        setMaster([...master , info ])
+        
+       }
+      
+       
+    console.log(master)
+      
+       console.log(master)
+        
+       
+     
+   
+       
+       
+       
+       
+          
+       
+       
+   
+     const calculate = (every, totalpoint) => {
+       console.log(calc)
+       console.log(calc2)
+        
+       
+       
+       setcalc(every)
+       setcalc2(totalpoint)
+    
+       
+       
+       
+     }
+     const finalresult = (calc2 / calc)
+     console.log(finalresult)
+     console.log(master)
+      
+     
+   
+     
+
+    
+    
+       return (
     <div>
        <Router>
       <Header/>
-      <Routes>
-         <Route exact path="/" element = {<Home/>}></Route>
-         <Route exact path="/comment" element = {<Comment/>}></Route>
-
-         <Route exact path="/about" element = {<About/>}></Route>
-
-         <Route exact path="/lesson" element = {<Lessons/>}></Route>
-
-         <Route exact path="/fulldetails/:id" element = {<Fulldetails/>}></Route>
-                  
-                  
-                    {/* dashboard */}
-         <Route exact path="/dashboard" element = {<DashboardApp/>}></Route>
-         <Route exact path="/post" element = {<Post/>}></Route>
-         <Route exact path="dashboard/editpost/:id" element = {<Editpost/>}></Route>
-         <Route exact path="/Comments" element = {<Comments/>}></Route>
-
-
-     
-      </Routes>
-     <Footer/>
-           
+           <Routes>
+             <Route exact path="/" element = {<Home/>}></Route>
+             <Route  exact path="/docs" element = {<Docs/>}></Route>
+             <Route exact path="/getstarted"  element = {<Getstarted/>}></Route>
+             <Route exact path='/about' element={<About/>}></Route>
+             <Route exact path="/result5"  element = 
+             {
+               <> 
+             <Gpcalc4    getinfo={getinfo} calculate={calculate}/>
+             <Result   master={master} calc={calc} calc2= {calc2} />
+              <Finalresult   finalresult={finalresult} />
+              
+              </>
+             }> 
+             
+             </Route>
             
+             <Route exact path="/result4"  element = 
+             {
+               <> 
+             <Gpcalc5   getinfo={getinfo} calculate={calculate}/>
+             <Result   master={master} calc={calc} calc2= {calc2} />
+              <Finalresult   finalresult={finalresult} />
+              
+              </>
+             }> 
+             
+             </Route>
+             
+             <Route exact path="/result7"  element = 
+             {
+               <> 
+             <Gpcalc7  getinfo={getinfo} calculate={calculate}/>
+             <Result   master={master} calc={calc} calc2= {calc2} />
+              <Finalresult   finalresult={finalresult} />
+              
+              </>
+             }> 
+             
+             </Route>
+           
+           </Routes>
 
           
       </Router>
@@ -60,7 +146,7 @@ function App() {
     
     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
